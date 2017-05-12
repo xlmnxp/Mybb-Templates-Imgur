bc70a11dab3932c6aac3e77e247cb5166506b373
xlmnxp <xlmnxp@outlook.sa>
2017-05-12 04:55:31 +0300

    Change The Number Settings

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+/**
+ * Error:
+ * خطأ عن الرفع خطئ واجهيي فقط لا غير
+ * في حالة الرفع لبعص العناصر في القائمة
+ * والتي تتمثل في ان العنصر يحمل 
+ * #num_# 
+ * # غير الذي في الـ 
+ */
+
 const imgur = require('imgur');
 var images_url = []
   , image_id   = []
@@ -17,8 +26,19 @@ String.prototype.replaceall = function(search, replacement) {
   return target.split(search).join(replacement);
 };
 
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
 function uploadall(_this){
-  getallformxml();
+  //getallformxml();
   $(_this).attr('disabled','disabled');
   $("#refreshbtn").attr('disabled','disabled');
 
@@ -78,8 +98,8 @@ function uploadall(_this){
 
 
 
-var num_errers = 0
-   ,num_succes = 0
+var num_err = 0
+   ,num_scu = 0
    ,__datatemp = [];
 function searchinfile(file) {
   var qualityRegex = /url\u0028(.*)\u0029/igm,
@@ -102,8 +122,8 @@ function searchinfile(file) {
 function getallformxml() {
   $("#viewer").html("");
   full_data = "";
-  num_succes = 0;
-  num_errers = 0;
+  num_scu = 0;
+  num_err = 0;
   images_url = [];
   __datatemp = [];
   fs.readFile('template.xml', 'utf8', function (err,data) {
@@ -118,15 +138,13 @@ function getallformxml() {
         fs.exists(value["url"], function(exists) {
           if( $.inArray(value["url"], __datatemp) < 0 ){
           if (exists) {
-            num_succes = num_succes + 1;
-            $("#viewer").html($("#viewer").html()+"<div oncontextmenu='func_contextmenu(this);' class='find_file find_yes' id='num_"+num_succes+"'><i class='ion-checkmark-circled'></i> <progress value='0' max='2'></progress> " + value["url"] + " <script type='json'>"+JSON.stringify(value)+"</script></div>");
+            num_scu = num_scu + 1;
+            $("#viewer").html($("#viewer").html()+"<div title='Image was found' oncontextmenu='func_contextmenu(this);' class='find_file find_yes' id='num_"+num_scu+"'><i class='ion-checkmark-circled'></i> <progress value='0' max='2'></progress> " + value["url"] + " <script type='json'>"+JSON.stringify(value)+"</script></div>");
             images_url.push(value["url"]);
           }else {
-            num_errers = num_errers + 1;
-            $("#viewer").html($("#viewer").html()+"<div oncontextmenu='func_contextmenu(this);' class='find_file find_no' id='"+value["url"]+"'><i class='ion-close-circled'></i> <progress style='background:silver;' disable=disable value='0' max='0'></progress> " + value["regex"] + " <script type='json'>"+JSON.stringify(value)+"</script></div>");
+            num_err = num_err + 1;
+            $("#viewer").html($("#viewer").html()+"<div title='No image found' oncontextmenu='func_contextmenu(this);' class='find_file find_no' id='"+value["url"]+"'><i class='ion-close-circled'></i> <progress style='background:silver;' disable=disable value='0' max='0'></progress> " + value["regex"] + " <script type='json'>"+JSON.stringify(value)+"</script></div>");
           }
-            $("#num_suc").html(num_succes);
-            $("#num_err").html(num_errers);
             __datatemp.push(value["url"]);
           }
           });
@@ -139,21 +157,53 @@ function func_contextmenu(element){
   const menu = new Menu();
   var self = element;
   var value = JSON.parse(self.querySelector('script').innerHTML);
-  menu.append(new MenuItem({label: 'Copy URL', click() { 
-    // copy item url found from list
-    clipboard.writeText(value['url']); 
-  }}));
-  menu.append(new MenuItem({label: 'Copy Regex', click() { 
-    // copy item regex found from list
-    clipboard.writeText(value['regex']); 
-  }}));
+  menu.append(new MenuItem(
+    {role: 'copy',
+    submenu: [
+      {label:"URL",click() { 
+        // copy item url found from list
+        clipboard.writeText(value['url']); 
+      }},
+      {label: 'Regex', click() { 
+        // copy item regex found from list
+        clipboard.writeText(value['regex']); 
+      }}
+  ] }));
   menu.append(new MenuItem({type: 'separator'}));
-  
-  menu.append(new MenuItem({label: 'Remove', click() { 
-    // remove item form list 
+  /*
+  menu.append(new MenuItem({role: 'delete', click() { 
+    switch(self.className){
+      case "find_file find_yes":
+        num_scu--;
+      break;
+      case "find_file find_no":
+        num_err--;
+      break;
+      default:
+        return;
+    }
+    images_url.remove(value["url"]);
+    self.parentElement.removeChild(self);
   }}));  
+
+  menu.append(new MenuItem({label: 'Delete All', click() { 
+    switch(self.className){
+      case "find_file find_yes":
+        num_scu--;
+      break;
+      case "find_file find_no":
+        num_err--;
+      break;
+      default:
+        return;
+    }
+    images_url.remove(value["url"]);
+    self.parentElement.removeChild(self);
+  }}));  
+  */
   menu.popup(remote.getCurrentWindow());
 }
+
 
 const InputMenu = Menu.buildFromTemplate([{
         label: 'Undo',
@@ -179,7 +229,10 @@ const InputMenu = Menu.buildFromTemplate([{
         role: 'selectall',
     },
 ]);
-
+setInterval(function(){
+  $("#num_suc").html(num_scu);
+  $("#num_err").html(num_err);
+},50);
 document.body.addEventListener('contextmenu', (e) => {
     e.preventDefault();
     e.stopPropagation();
